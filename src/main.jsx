29f2d6e2aeadc696bc3b775d2842f4808cf79534
tomaspadran <tomaspadran@gmail.com>
2026-01-04 11:18:54 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ThemeProvider } from "./components/theme-provider"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 )
