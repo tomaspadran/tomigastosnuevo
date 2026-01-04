64ee3bf548909a64cef3acf787dee0d883c881df
tomaspadran <tomaspadran@gmail.com>
2026-01-04 10:47:48 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Reemplaza los valores de abajo con los que sacaste de tu panel de Supabase
-const supabaseUrl = 'https://hururbfcotnebgamhget.supabase.co'
-const supabaseAnonKey = 'sb_publishable_hSrl7qXqO3l5kzuYO19yYQ_2OYOiXxH'
+const supabaseUrl = 'TU_URL'
+const supabaseAnonKey = 'TU_KEY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
