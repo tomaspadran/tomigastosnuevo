9a923ea915e576daa77050d22b3feb1ebc14be2b
tomaspadran <tomaspadran@gmail.com>
2026-01-04 13:19:22 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // ASEGÚRATE DE QUE LA URL EMPIECE CON HTTPS://
-const supabaseUrl = 'https://tu-proyecto.supabase.co' 
-const supabaseAnonKey = 'tu-clave-larga-aqui'
+const supabaseUrl = 'https://hururbfcotnebgamhget.supabase.co' 
+const supabaseAnonKey = 'sb_publishable_hSrl7qXqO3l5kzuYO19yYQ_2OYOiXxH'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
