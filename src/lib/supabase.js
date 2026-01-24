492a8e5829d76bbe930517f4d5c122bd993c0096
tomaspadran <tomaspadran@gmail.com>
2026-01-24 10:55:41 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,6 +5,6 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://hururbfcotnebg
 
 // NOTA: Asegúrate de que esta Key sea la "anon public" que copiaste de Supabase.
 // La que pasaste se veía duplicada, aquí la dejo limpia si es que era un error de pegado.
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_hSrl7qXqO3l5kzuYO19yYQ_2OYOiXxH'; 
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1cnVyYmZjb3RuZWJnYW1oZ2V0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0NTEzNDcsImV4cCI6MjA4MzAyNzM0N30.OTzen7ePLhG036uK4grHNsZoYfo2oq7RPUrTCSVr33k'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
