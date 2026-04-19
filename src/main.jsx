2fda282bac8e8edcaa2970f8b0b5bca4b254dcb6
tomaspadran <tomaspadran@gmail.com>
2026-04-18 23:42:28 -0300

    appformato

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+// Registro automático del Service Worker para PWA
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
